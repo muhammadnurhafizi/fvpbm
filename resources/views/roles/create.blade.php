527529f436b6898759ba8960ad9ea2273dbbc480
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-14 21:03:17 +0800

    fix create role page

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -8,22 +8,25 @@
     
     <div class="row">
         <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <form action="{{ route('roles.store') }}" method="post">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Role Name</label>
-                            <input type="text" class="form-control" id="name" name="name" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="description" class="form-label">Description</label>
-                            <input type="text" class="form-control" id="description" name="description" required>
-                        </div>
-                        <button class="btn btn-primary" type="submit">Submit</button>
-                    </form>
+            <form action="{{ route('roles.store') }}" method="post">
+                @csrf
+                <div class="card">
+                    <div class="card-body">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Role Name</label>
+                                <input type="text" class="form-control" id="name" name="name" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Description</label>
+                                <input type="text" class="form-control" id="description" name="description" required>
+                            </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <a href="{{ route('roles.index') }}" class="btn btn-outline-secondary"><i class="fas fa-arrow-left"></i> Back</a>
+                        <button class="btn btn-success" type="submit"><i class="fas fa-check"></i> Save</button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 
