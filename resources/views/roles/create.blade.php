6188f7d0522dcb64b870a7532ae81581eab545d3
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-14 17:32:12 +0800

    add create role page

---
+++ resources/views/roles/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title')
+    Create a new role
+@endsection
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('roles.store') }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Role Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <input type="text" class="form-control" id="description" name="description" required>
+                        </div>
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+    
+@endsection
