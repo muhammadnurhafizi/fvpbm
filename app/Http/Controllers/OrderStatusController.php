6abb9d510ded56b2af6ed7e82ab41166d1e2d08f
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-17 02:59:41 +0800

    create order status controller

---
+++ app/Http/Controllers/OrderStatusController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderStatus;
+use Illuminate\Http\Request;
+
+class OrderStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+        $orderStatuses = OrderStatus::where([
+            [function ($query) use ($request) {
+                if (($term = $request->keyword)) {
+                    $query->orWhere('name', 'LIKE', '%' . $term . '%')
+                        ->orWhere('description', 'LIKE', '%' . $term . '%')
+                        ->get();
+                }
+            }]
+        ])
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('orderStatuses.index', compact('keyword', 'orderStatuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('orderStatuses.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {        
+        $orderStatus = new OrderStatus;
+        $orderStatus->name = strtolower($request->name);
+        $orderStatus->description = strtolower($request->description);
+        $orderStatus->save();
+
+        return redirect()->route('orderStatuses.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrderStatus  $orderStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderStatus $orderStatus)
+    {
+        return view('orderStatuses.show', compact('orderStatus'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OrderStatus  $orderStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrderStatus $orderStatus)
+    {
+        return view('orderStatuses.edit', compact('orderStatus'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrderStatus  $orderStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderStatus $orderStatus)
+    {
+        $orderStatus->name = strtolower($request->name);
+        $orderStatus->description = strtolower($request->description);
+        $orderStatus->update();
+
+        return redirect()->route('orderStatuses.show', compact('orderStatus'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrderStatus  $orderStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderStatus $orderStatus)
+    {
+        $orderStatus->delete();
+
+        return redirect()->route('orderStatuses.index');
+    }
+}
