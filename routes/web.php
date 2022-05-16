80a32c2b27c6fd79abb850d073071b01f56836eb
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-16 20:23:35 +0800

    create agency routes

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::resource('users', App\Http\Controllers\UserController::class);
 Route::resource('clinics', App\Http\Controllers\ClinicController::class);
 Route::resource('hospitals', App\Http\Controllers\HospitalController::class);
 Route::resource('frequencies', App\Http\Controllers\FrequencyController::class);
+Route::resource('agencies', App\Http\Controllers\AgencyController::class);
