076d87d8384dd2491476d83b2f06b816f78df95a
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-16 15:50:59 +0800

    create clinic controller

---
+++ app/Http/Controllers/ClinicController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Clinic;
+use Illuminate\Http\Request;
+
+class ClinicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+        $clinics = Clinic::where([
+            [function ($query) use ($request) {
+                if (($term = $request->keyword)) {
+                    $query->orWhere('name', 'LIKE', '%' . $term . '%')
+                        ->orWhere('description', 'LIKE', '%' . $term . '%')
+                        ->get();
+                }
+            }]
+        ])
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('clinics.index', compact('keyword', 'clinics'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clinics.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $clinic = new Clinic;
+        $clinic->name = strtolower($request->name);
+        $clinic->description = strtolower($request->description);
+        $clinic->save();
+
+        return redirect()->route('clinics.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Clinic  $clinic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Clinic $clinic)
+    {
+        return view('clinics.show', compact('clinic'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Clinic  $clinic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Clinic $clinic)
+    {
+        return view('clinics.edit', compact('clinic'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Clinic  $clinic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Clinic $clinic)
+    {
+        $clinic->name = strtolower($request->name);
+        $clinic->description = strtolower($request->description);
+        $clinic->update();
+
+        return redirect()->route('clinics.show', compact('clinic'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Clinic  $clinic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Clinic $clinic)
+    {
+        $clinic->delete();
+
+        return redirect()->route('clinics.index');
+    }
+}
