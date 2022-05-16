055dcc935cf145957393d1d7bc9df23ba2cc5ea0
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-16 16:18:52 +0800

    create hospital controller

---
+++ app/Http/Controllers/HospitalController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hospital;
+use Illuminate\Http\Request;
+
+class HospitalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+        $hospitals = Hospital::where([
+            [function ($query) use ($request) {
+                if (($term = $request->keyword)) {
+                    $query->orWhere('name', 'LIKE', '%' . $term . '%')
+                        ->orWhere('description', 'LIKE', '%' . $term . '%')
+                        ->get();
+                }
+            }]
+        ])
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('hospitals.index', compact('keyword', 'hospitals'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('hospitals.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $hospital = new Hospital;
+        $hospital->name = strtolower($request->name);
+        $hospital->description = strtolower($request->description);
+        $hospital->save();
+
+        return redirect()->route('hospitals.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hospital  $hospital
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hospital $hospital)
+    {
+        return view('hospitals.show', compact('hospital'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Hospital  $hospital
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hospital $hospital)
+    {
+        return view('hospitals.edit', compact('hospital'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Hospital  $hospital
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hospital $hospital)
+    {
+        $hospital->name = strtolower($request->name);
+        $hospital->description = strtolower($request->description);
+        $hospital->update();
+
+        return redirect()->route('hospitals.show', compact('hospital'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hospital  $hospital
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hospital $hospital)
+    {
+        $hospital->delete();
+
+        return redirect()->route('hospitals.index');
+    }
+}
