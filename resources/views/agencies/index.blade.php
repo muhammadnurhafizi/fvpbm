48ecafc2174fadf5e49870dfe55001a22aacf420
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-16 20:23:59 +0800

    create agency pages

---
+++ resources/views/agencies/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('title')
+    Agencies
+@endsection
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <form action="{{ route('agencies.index') }}" method="get">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="keyword" placeholder="Search..." value="{{ $keyword }}">
+                            <button class="btn btn-outline-secondary"><i class="fas fa-search"></i></button>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table text-nowrap align-middle">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                @if (sizeOf($agencies))
+                                    @foreach ($agencies as $agency)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td><a href="{{ route('agencies.show', ["agency"=>$agency->id]) }}" class="text-decoration-none">{{ ucwords($agency->name) }}</a></td>
+                                            <td>{{ ucwords($agency->description) }}</td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="3" class="text-center py-3 border-0"><span class="fw-bold text-secondary">Data not found</span></td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                        <div class="d-flex justify-content-center">
+                            {!! $agencies->links() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="{{ route('agencies.create') }}" class="btn btn-primary float-end"><i class="fas fa-plus"></i> Create a new agency</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+    
+@endsection
