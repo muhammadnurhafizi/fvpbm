963b58bd4e96b858894217c82821574161fe0b8f
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-27 23:31:25 +0800

    create state controller

---
+++ app/Http/Controllers/StateController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\State;
+use Illuminate\Http\Request;
+
+class StateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+        $states = State::where([
+            [function ($query) use ($request) {
+                if (($term = $request->keyword)) {
+                    $query->orWhere('name', 'LIKE', '%' . $term . '%')
+                        ->get();
+                }
+            }]
+        ])
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('states.index', compact('keyword', 'states'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('states.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $state = new State;
+        $state->name = strtolower($request->name);
+        $state->save();
+
+        return redirect()->route('states.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function show(State $state)
+    {
+        return view('states.show', compact('state'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(State $state)
+    {
+        return view('states.edit', compact('state'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, State $state)
+    {
+        $state->name = strtolower($request->name);
+        $state->update();
+
+        return redirect()->route('states.show', compact('state'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(State $state)
+    {
+        $state->delete();
+
+        return redirect()->route('states.index');
+    }
+}
