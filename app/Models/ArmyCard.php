0e6a2c65474c8b1dbd91f9f2d03a2fd4ee10f73e
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-29 19:12:50 +0800

    create army card model

---
+++ app/Models/ArmyCard.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ArmyCard extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function veteran_status() {
+        return $this->belongsTo(VeteranStatus::class);
+    }
+
+    public function agency() {
+        return $this->belongsTo(Agency::class);
+    }
+
+    public function army_type() {
+        return $this->belongsTo(ArmyType::class);
+    }
+}
