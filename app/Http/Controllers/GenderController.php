8be33e850db312fd9c3e84817e3ec0571e6c4ec3
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-30 12:05:04 +0800

    create gender controller

---
+++ app/Http/Controllers/GenderController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gender;
+use Illuminate\Http\Request;
+
+class GenderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->keyword;
+        $genders = Gender::where([
+            [function ($query) use ($request) {
+                if (($term = $request->keyword)) {
+                    $query->orWhere('name', 'LIKE', '%' . $term . '%')
+                        ->orWhere('description', 'LIKE', '%' . $term . '%')
+                        ->get();
+                }
+            }]
+        ])
+            ->orderBy('name')
+            ->paginate(15);
+
+        return view('genders.index', compact('keyword', 'genders'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('genders.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $gender = new Gender;
+        $gender->name = strtolower($request->name);
+        $gender->description = strtolower($request->description);
+        $gender->save();
+
+        return redirect()->route('genders.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gender $gender)
+    {
+        return view('genders.show', compact('gender'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gender $gender)
+    {
+        return view('genders.edit', compact('gender'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gender $gender)
+    {
+        $gender->name = strtolower($request->name);
+        $gender->description = strtolower($request->description);
+        $gender->update();
+
+        return redirect()->route('genders.show', compact('gender'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gender  $gender
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gender $gender)
+    {
+        $gender->delete();
+
+        return redirect()->route('genders.index');
+    }
+}
