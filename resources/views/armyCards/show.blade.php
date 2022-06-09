887decdaed25fca5a86dc33211049be0e32168f7
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-06-09 17:24:34 +0800

    display card owner and relative in show army card page

--- resources/views/armyCards/show.blade.php
+++ resources/views/armyCards/show.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     
-    <div class="row">
+    <div class="row mb-3">
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
@@ -56,6 +56,120 @@
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Card Owner</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table text-nowrap align-middle">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Full Name</th>
+                                    <th>NRIC</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $num = 0;
+                                @endphp
+
+                                @if (sizeOf($armyCard->relatives))
+                                    @foreach ($armyCard->relatives as $relative)
+                                        @if ($relative->patient_relation_id == 1)
+                                            @php
+                                                $cardOwner = $relative->patient;
+                                            @endphp
+
+                                            <tr>
+                                                <td>{{ ++$num }}</td>
+                                                <td><a href="{{ route('armyCards.patients.show', ["armyCard"=>$armyCard->id, "patient"=>$cardOwner->id]) }}">{{ ucwords($cardOwner->full_name) }}</a></td>
+                                                <td>{{ $cardOwner->nric }}</td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+
+                                    @if ($num == 0)
+                                        <tr>
+                                            <td colspan="3" class="text-center py-3 border-0"><span class="fw-bold text-secondary">Data not found</span></td>
+                                        </tr>
+                                    @endif
+                                @else
+                                    <tr>
+                                        <td colspan="3" class="text-center py-3 border-0"><span class="fw-bold text-secondary">Data not found</span></td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                @if ($num == 0)
+                    <div class="card-footer">
+                        <a href="{{ route('armyCards.patients.create', ['armyCard'=>$armyCard->id]) }}" class="btn btn-primary float-end"><i class="fas fa-plus"></i> Add Card Owner</a>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Relatives</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table text-nowrap align-middle">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Full Name</th>
+                                    <th>NRIC</th>
+                                    <th>Relation</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $num = 0;
+                                @endphp
+
+                                @if (sizeOf($armyCard->relatives))
+                                    @foreach ($armyCard->relatives as $relative)
+                                        @if ($relative->patient_relation_id !== 1)
+                                            <tr>
+                                                <td>{{ ++$num }}</td>
+                                                <td><a href="{{ route('armyCards.patients.show', ["armyCard"=>$armyCard->id, "patient"=>$relative->patient->id]) }}">{{ ucwords($relative->patient->full_name) }}</a></td>
+                                                <td>{{ $relative->patient->nric }}</td>
+                                                <td>{{ ucwords($relative->patient_relation->name) }}</td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+
+                                    @if ($num == 0)
+                                        <tr>
+                                            <td colspan="4" class="text-center py-3 border-0"><span class="fw-bold text-secondary">Data not found</span></td>
+                                        </tr>
+                                    @endif
+                                @else
+                                    <tr>
+                                        <td colspan="4" class="text-center py-3 border-0"><span class="fw-bold text-secondary">Data not found</span></td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="{{ route('armyCards.patients.create', ['armyCard'=>$armyCard->id, 'relative'=>1]) }}" class="btn btn-primary float-end"><i class="fas fa-plus"></i> Add Relative</a>
                 </div>
             </div>
         </div>
