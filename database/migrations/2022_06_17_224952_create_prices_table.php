6a221031001a972def63419190303ba366d78768
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-06-24 00:06:17 +0800

    create price table

---
+++ database/migrations/2022_06_17_224952_create_prices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount')->default(0.00);
+            $table->string('description')->nullable();
+            $table->foreignId('item_id')->constrained();
+            $table->foreignId('price_type_id')->constrained();
+            $table->foreignId('uom_id')->constrained();
+            $table->dateTime('started_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+};
