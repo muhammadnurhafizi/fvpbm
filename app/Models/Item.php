d1a1166acac19b3c9115f141e9114c9fce390584
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-06-23 14:45:59 +0800

    create item, item_package and stock models

---
+++ app/Models/Item.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function instruction() {
+        return $this->belongsTo(Instruction::class);
+    }
+
+    public function frequency() {
+        return $this->belongsTo(Frequency::class);
+    }
+
+    public function quantity_formula() {
+        return $this->belongsTo(QuantityFormula::class);
+    }
+
+    public function item_package() {
+        return $this->hasOne(ItemPackage::class);
+    }
+
+    public function stock() {
+        return $this->hasOne(Stock::class);
+    }
+}
